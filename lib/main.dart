1492243a12bb5daa75492e4dff57e26c97935bb3
Ahmet YILDIRIM <ahmetyildirim3@gmail.com>
2019-01-24 13:44:15 +0300

    include firebase message

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,51 @@ import 'package:flutter/material.dart';
 import 'package:talker_app/common/constants.dart';
 import 'package:talker_app/pages/login.dart';
 // import 'package:flutter/rendering.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 void main() {
   // debugPaintSizeEnabled = true;
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String textValue = "Hello worldd";
+  FirebaseMessaging firebaseMessaging = FirebaseMessaging();
+  update(String token) {
+    print(token);
+    textValue = token;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    firebaseMessaging.configure(
+      onLaunch: (Map<String, dynamic> msg) {
+        print('ON-LAUNCH called');
+      },
+      onResume: (Map<String, dynamic> msg) {
+        print('ON-RESUME called');
+      },
+      onMessage: (Map<String, dynamic> msg) {
+        print('ON-MESSAGE called');
+      },
+    );
+    firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(sound: true, alert: true, badge: true));
+    firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {
+      print('IOS settings registered');
+    });
+    firebaseMessaging.getToken().then((token) {
+      update(token);
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,8 +55,6 @@ class MyApp extends StatelessWidget {
         primaryColor: themeColor,
       ),
       home: LoginPage(),
-      
     );
   }
 }
-
